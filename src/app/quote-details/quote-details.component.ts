00cef622c18ac95fc99a7a1dc51f445ae46f4aea
vincentmuya <vincentmuya13@gmail.com>
2018-01-05 17:00:54 +0300

    made modification to code

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
 import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-details',
@@ -7,6 +7,10 @@ import { Quote } from '../quote'
 })
 export class QuoteDetailsComponent implements OnInit {
 @Input()quote:Quote
+@Output()isComplete= new EventEmitter<boolean>()
+quoteDelete(complete:boolean){
+  this.isComplete.emit(complete);
+}
   constructor() { }
 
   ngOnInit() {
