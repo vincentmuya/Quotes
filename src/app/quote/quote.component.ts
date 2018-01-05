d6d01f387a8293cfadd05f41975107120c0a46cb
vincentmuya <vincentmuya13@gmail.com>
2018-01-05 13:03:15 +0300

    added forms and pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
 quotes = [
-  new Quote(1,"Dr. Seuss","Don't cry because it's over, smile because it happened."),
-  new Quote(2,"Oscar Wilde", "Be yourself; everyone else is already taken."),
-  new Quote(3," Marilyn Monroe", "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."),
-  new Quote(4,"Albert Einstein", "Albert Einstein"),
-  new Quote(5,"Marcus Tullius Cicero", "A room without books is like a body without a soul."),
-  new Quote(6,"Bernard M. Baruch", "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.")
+  new Quote(1,"Dr. Seuss","Don't cry because it's over, smile because it happened.", new Date(2017,11,15)),
+  new Quote(2,"Oscar Wilde", "Be yourself; everyone else is already taken.", new Date(2018,1.1)),
+  new Quote(3," Marilyn Monroe", "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", new Date(2018,1,3)),
+  new Quote(4,"Albert Einstein", "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", new Date(2018,1.5)),
+  new Quote(5,"Marcus Tullius Cicero", "A room without books is like a body without a soul.",new Date(2018,1.7)),
+  new Quote(6,"Bernard M. Baruch", "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.", new Date(2018,1.9))
 ]
   constructor() { }
 
