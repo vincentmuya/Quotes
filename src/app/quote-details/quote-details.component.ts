afbd053eb0ad104f792a2aa45878a087a747fdd0
vincentmuya <vincentmuya13@gmail.com>
2018-01-06 12:45:02 +0300

    made modificxation to code

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -8,8 +8,9 @@ import { Quote } from '../quote'
 export class QuoteDetailsComponent implements OnInit {
 @Input()quote:Quote
 @Output()isComplete= new EventEmitter<boolean>()
+
 quoteDelete(complete:boolean){
-this.isComplete.emit(complete);
+  this.isComplete.emit(complete);
 }
   constructor() { }
 
